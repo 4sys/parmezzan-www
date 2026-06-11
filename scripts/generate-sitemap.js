b05ea7eef5a94f9cf00e68b89417bfc8f20912af
Piotr Zaniewski <pz@4sys.pl>
2026-06-11 12:03:52 +0200

    Temporarily disable website per client request

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -3,12 +3,7 @@ const { createWriteStream } = require("fs");
 const { url } = require("inspector");
 
 const links = [
-  { url: '/',  changefreq: 'daily', priority: 1, lastmod: new Date().toISOString().slice(0, 16) },
-  { url: '/menu',  changefreq: 'daily', priority: 1, lastmod: new Date().toISOString().slice(0, 16) },
-  { url: '/en',  changefreq: 'daily', priority: 1, lastmod: new Date().toISOString().slice(0, 16) },
-  { url: '/en/menu',  changefreq: 'daily', priority: 1, lastmod: new Date().toISOString().slice(0, 16) },
-  { url: '/it',  changefreq: 'daily', priority: 1, lastmod: new Date().toISOString().slice(0, 16) },
-  { url: '/it/menu',  changefreq: 'daily', priority: 1, lastmod: new Date().toISOString().slice(0, 16) },
+  { url: '/',  changefreq: 'daily', priority: 1, lastmod: new Date().toISOString().slice(0, 16) }
 ]
 
 let sitemap = new SitemapStream({ hostname: "https://parmezzan.pl" });
